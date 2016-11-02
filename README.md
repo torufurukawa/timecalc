ac2e7eee1cf26f274f7c28604443c58193ade313
Toru Furukawa <torufurukawa@gmail.com>
2016-11-02 16:10:05 +0900

    Add stub

---
+++ README.md
@@ -0,0 +1,15 @@
+# timecalc - Time Calculator
+
+Calculator for times
+
+## Requirements
+
+- Node 6.9.1
+
+## Usage
+
+To run stub script
+
+```
+npm run main
+```
---
+++ main.ts
@@ -0,0 +1,15 @@
+console.log("Hello world!");
+
+//
+// calculate "4:00:00 / 42.2" => 0:05:41.2
+//
+let y = 42.2;
+
+// 4:00:00 => 14400
+let x = 14400;
+
+// 14400 / 42.2
+let result = x / y;
+console.log(result);
+
+// X => 0:05:41.2
