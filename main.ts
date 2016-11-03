0dc39cb5f0269778655e69905ea1d15ba4ba4e89
Toru Furukawa <torufurukawa@gmail.com>
2016-11-04 08:27:54 +0900

    Add parsing duration

--- main.ts
+++ main.ts
@@ -7,10 +7,12 @@ console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
 let y = 42.2;
 
 // 4:00:00 => 14400
-let x = 14400;
+let x = moment.duration('4:00:00').asSeconds();
 
 // 14400 / 42.2
 let result = x / y;
 console.log(result);
 
-// X => 0:05:41.2
+// result => 0:05:41.2
+let result_duration = moment.duration(result * 1000);
+console.log(result_duration.format('hh:mm:ss')); // error
